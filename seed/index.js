9c657714877f7593647e5f511aa36d64a8b708b6
Mark Harmon <neothej1sec@gmail.com>
2022-04-06 13:13:01 -0500

    new seed data

--- seed/index.js
+++ seed/index.js
@@ -7,12 +7,24 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
   const post1 = new Post({
-    name: 'George',
-    review: 'So good',
-    location: 'Boston, MA',
-    image:
-      'https://assets.simpleviewinc.com/simpleview/image/fetch/c_limit,h_1200,q_75,w_1200/https://assets.simpleviewinc.com/simpleview/image/upload/crm/boston/bpl0_25978b77-5056-a36a-0648935cebef87c5.jpg'
+    name: 'Saint Petersburg',
+    review: 'Saint Petersburg formerly known as Petrograd (1914-1924) and later Leningrad (1924-1991), is the second-largest city in Russia. The city was founded by Tsar Peter the Great on 27 May 1703 on the site of a captured Swedish fortress, and was named after apostle Saint Peter.',
+    location: 'Saint Petersburg, Russia',
+    image: 'https://www.jetsetter.com//uploads/sites/7/2018/04/nEtTvYR3.jpeg'
   })
+  const post2 = new Post({
+      name: 'Thailand',
+      review: 'Thailand historically known as Siam is home to the capitol Bangkok.Following a bloodless revolution in 1932, it became a constitutional monarchy and changed its official name to Thailand, which was an ally of Japan in World War II. ',
+      location: 'Southeast Asia',
+      image: 'https://lp-cms-production.imgix.net/2021-03/GettyRF_178111904.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850'
+  })
+  const post3 = new Post({
+      name: 'Italy',
+      review: ' Italy is located in the middle of the Mediterranean Sea, in Southern Europe; it is also considered part of Western Europe. During the Early Middle Ages, Italy endured the fall of the Western Roman Empire and the Barbarian Invasions, but by the 11th century numerous rival city-states and maritime republics, mainly in the northern and central regions of Italy, became prosperous through trade, commerce, and banking, laying the groundwork for modern capitalism.',
+      location: 'Europe',
+      image: 'https://deih43ym53wif.cloudfront.net/amalfi-italy-shutterstock_759048709_bdda191300.jpeg'
+  })
+
 
   const comment1 = new Comment({
     rating: 5,
@@ -28,6 +40,8 @@ const main = async () => {
   await comment1.save()
   await comment2.save()
   await post1.save()
+  await post2.save()
+  await post3.save()
 
   console.log('Created some posts!')
 }
