47cc9c0cec47f7e17e29722ef4eba8da20b710a2
LawrenceOL <loreganlloyd@gmail.com>
2022-04-06 19:56:31 -0400

    revised axios

--- db/index.js
+++ db/index.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/<database_name>'
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/travelBlogDatabase')
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
