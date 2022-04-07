97a6085d73c650f1fb984491b5e9d646d51ccbb5
LawrenceOL <loreganlloyd@gmail.com>
2022-04-06 20:02:49 -0400

    merged css updates

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,32 +3,28 @@ import { LoadPosts } from '../store/actions/PostActions'
 import { useEffect } from 'react'
 import Post from '../components/Post'
 
-
-
 const mapStateToProps = ({ postState }) => {
-    return { postState }
+  return { postState }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchPosts: () => dispatch(LoadPosts())
-    }
+  return {
+    fetchPosts: () => dispatch(LoadPosts())
+  }
 }
 
-const HomePage = (props) => { 
-    
-    useEffect(() => {
-        props.fetchPosts()
-    }, [])
-    
-    
-    return (
-        <div>
-            {props.postState.posts.map((post) => (
-                <Post key={post._id} post={post} />
-            ))}
-        </div>
-    )
+const HomePage = (props) => {
+  useEffect(() => {
+    props.fetchPosts()
+  }, [])
+
+  return (
+    <div className="home">
+      {props.postState.posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </div>
+  )
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
